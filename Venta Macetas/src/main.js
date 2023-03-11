92a718cc86abdb658457191613536695f604e1a0
Pablo_Parisi <parisipm@gmail.com>
2023-03-11 15:35:49 -0300

    Se implementan cambios en los códigos: Nomenclatura, imágenes y secciones

--- Venta Macetas/src/main.js
+++ Venta Macetas/src/main.js
@@ -1,10 +1,10 @@
-const categorias = document.querySelector(".Categorias");
+const categorias = document.querySelector(".CatalogoCategorias");
 
 function crearMaceta(nombre,modelo,precio){
-    img = `<img class='maceta-img' src='https://img.freepik.com/foto-gratis/planta-nido-pajaros-maceta-beige_53876-134284.jpg?w=740&t=st=1678307227~exp=1678307827~hmac=dfb9cb9374e1eec722cbe578e279df6aa8630020ab1df1ed32a33812756b413d'>`;
-    nombre = `<p class="maceta-texto">${nombre}</p>`;
-    modelo = `<p class="maceta-texto">${modelo}</p>`;
-    precio = `<p class="maceta-texto precio">${precio}</p>`;
+    img = `<img class='MacetaImg' src='https://img.freepik.com/foto-gratis/planta-nido-pajaros-maceta-beige_53876-134284.jpg?w=740&t=st=1678307227~exp=1678307827~hmac=dfb9cb9374e1eec722cbe578e279df6aa8630020ab1df1ed32a33812756b413d'>`;
+    nombre = `<p class="MacetaTexto">${nombre}</p>`;
+    modelo = `<p class="MacetaTexto">${modelo}</p>`;
+    precio = `<p class="MacetaTexto Precio">${precio}</p>`;
     return [img,nombre,modelo,precio];
 }
 
@@ -15,7 +15,7 @@ for(var i = 1;i <= 12; i++){
     const preciorandom = Math.round(Math.random()*10 + 100);
     const maceta =crearMaceta(`Maceta ${i}`,`Modelo M-${modelorandom}`,`$${preciorandom}`);
     const item = document.createElement("DIV");
-    item.classList.add(`flex-muestras`); //Agrego una clase al ítem
+    item.classList.add(`Muestras`); //Agrego una clase al ítem
     item.innerHTML = maceta[0] + maceta[1] + maceta[2] + maceta[3];
     fragmento.appendChild(item);
 }
